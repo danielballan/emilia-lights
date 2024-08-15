1a22ead8d5095c3e56830de5600a956782e18e46
Dan Allan <daniel.b.allan@gmail.com>
2024-08-15 09:42:02 -0400

    First commit after many changes over months

--- code.py
+++ code.py
@@ -8,10 +8,11 @@ import neopixel
 NUM_PIXELS = 100
 NUM_SPARKLES = 10
 TIME_STEP = 0.1  # seconds
-HIGH_BRIGHTNESS = 1
-LOW_BRIGHTNESS = 0.03
+TIMEOUT = 30 * 50 / TIME_STEP  # 30 minutes
+HIGH_BRIGHTNESS = 0.9  # Set below max of 1 to reduce flickering.
+LOW_BRIGHTNESS = 0.02  # Color depth is a little better than just straight R/G/B.
 
-led = neopixel.NeoPixel(board.NEOPIXEL, 1, auto_write=False)
+led = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.02, auto_write=False)
 strings = [
     neopixel.NeoPixel(board.D11, NUM_PIXELS, auto_write=False),
     neopixel.NeoPixel(board.D10, NUM_PIXELS, auto_write=False),
@@ -85,38 +86,66 @@ BLACK_CANVAS = [(0, 0, 0) for _ in range(100)]
 canvas = BLACK_CANVAS.copy()
 # Track the positions of "sparkle" pixels.
 sparkle_layer = [random.randint(0, NUM_PIXELS - 1) for _ in range(3 * NUM_SPARKLES)]
+SWITCH_REPR = {True: "↑", False: "↓"}
+
+time_steps_since_last_touched = 0
+status_quo = [False, False, False, False]
 while True:
-    # Extract the state of each switch.
-    switch_values = [switch.value for switch in switches]
-    # Switch #4 controls brightness.
-    brightness = HIGH_BRIGHTNESS if switch_values[3] else LOW_BRIGHTNESS
-    # The arrangement of Switch #1 and Switch #2 together choose a pattern.
-    # The on-board LED can be used for debugging.
-    # Note that the on-board LED is RGB, while the strings are GRB.
-    if switch_values[:2] == [True, True]:
-        led[0] = (0, 255, 0)  # green
-        canvas[:] = RAINBOW_CANVAS
-    elif switch_values[:2] == [True, False]:
-        led[0] = (255, 0, 0)  # red
-        canvas[:] = RED_AND_BLUE_AND_PURPLE_AND_PINK_CANVAS
-    elif switch_values[:2] == [False, True]:
-        led[0] = (0, 0, 255)  # blue
-        canvas[:] = ELSA_MAGIC_CANVAS
-    else:
-        led[0] = (0, 0, 0)
-        canvas[:] = BLACK_CANVAS
-    for i, string in enumerate(strings):
-        string.brightness = brightness
-        string[:] = canvas
-        # If Switch #3 is on, draw a sparkling effect over top of canvas
-        # (unless the canvas is black).
-        if switch_values[2] and any(switch_values[:2]):
-            for j in sparkle_layer[(NUM_SPARKLES * i) : (NUM_SPARKLES * (1 + i))]:
-                string[j] = (255, 255, 255)
-    led.show()
-    for string in strings:
-        string.show()
-    time.sleep(TIME_STEP)
-    # Remove one "sparkle" pixel and add one new one.
-    sparkle_layer.pop(0)
-    sparkle_layer.append(random.randint(0, NUM_PIXELS - 1))
+    for tick in range(255):
+        time_steps_since_last_touched += 1
+        # Extract the state of each switch.
+        switch_values = [switch.value for switch in switches]
+        if switch_values != status_quo:
+            time_steps_since_last_touched = 0
+        # Switch #4 controls brightness.
+        brightness = HIGH_BRIGHTNESS if switch_values[3] else LOW_BRIGHTNESS
+        # The arrangement of Switch #1 and Switch #2 together choose a pattern.
+        # The on-board LED can be used for debugging.
+        # Note that the on-board LED is RGB, while the strings are GRB.
+        mode = None
+        if switch_values[:3] == [True, True, True]:
+            mode = "rainbow wheel"
+            for i in range(NUM_PIXELS):
+                COMPRESS = 3  # Higher compresses more rainbow onto string.
+                canvas[i] = wheel((i + COMPRESS * tick) % 255)
+        elif switch_values[:2] == [True, True]:
+            mode = "random rainbow"
+            canvas[:] = RAINBOW_CANVAS
+        elif switch_values[:2] == [True, False]:
+            mode = "red & blue & purple & pink"
+            canvas[:] = RED_AND_BLUE_AND_PURPLE_AND_PINK_CANVAS
+        elif switch_values[:2] == [False, True]:
+            mode = "frozen"
+            canvas[:] = ELSA_MAGIC_CANVAS
+        else:
+            mode = "black"
+            canvas[:] = BLACK_CANVAS
+        if time_steps_since_last_touched > TIMEOUT:  # 10 minutes
+            canvas[:] = BLACK_CANVAS
+        # Draw canvas onto each string.
+        for i, string in enumerate(strings):
+            string.brightness = brightness
+            string[:] = canvas
+        # If Switch #3 is on and ONE of Switch #1 or #2 is on,
+        # layer a sparkling effect over top of canvas.
+        if (time_steps_since_last_touched <= TIMEOUT) and switch_values[2] and (0 < sum(switch_values[:2]) < 2):
+            mode += " +sparkle"
+            for i, string in enumerate(strings):
+                for j in sparkle_layer[(NUM_SPARKLES * i) : (NUM_SPARKLES * (1 + i))]:
+                    string[j] = (255, 255, 255)
+        # Update indicator LED.
+        led[0] = (255 * int(switch_values[0]), 255 * int(switch_values[1]), 0)
+        if led[0][:2] == (0, 0):
+            led[0] = (0, 0, 255)
+        # Draw.
+        led.show()
+        for string in strings:
+            string.show()
+        # print(" ".join(SWITCH_REPR[value] for value in switch_values), mode)
+        # Sleep for 1 / frame rate
+        time.sleep(TIME_STEP)
+        # Remove one "sparkle" pixel and add one new one.
+        sparkle_layer.pop(0)
+        sparkle_layer.append(random.randint(0, NUM_PIXELS - 1))
+        print(switch_values, status_quo, time_steps_since_last_touched)
+        status_quo = switch_values
